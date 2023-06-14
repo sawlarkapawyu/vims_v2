ca4409d3d7ca3d14843f0d144324b8ef2ad0ce43
st122632 <109932738+st122632@users.noreply.github.com>
2023-06-14 14:41:18 +0700

    Edited profile page

--- src/components/users/Profile.jsx
+++ src/components/users/Profile.jsx
@@ -72,87 +72,92 @@ export default function UserProfile( { session } ) {
 
     return (
         
-        <div className="space-y-10 divide-y divide-gray-900/10">
-            <div className="">
-                
-                <form className="shadow-sm bg-gray-50 ring-1 ring-gray-200 sm:rounded-xl">
-              
-                    <div class="grid grid-rows-3 grid-flow-col gap-4">
-                        <div class="col-span-2 px-8 py-8">
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                {t("profile.Email")}
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    id="email" 
-                                    type="text" 
-                                    value={session.user.email} 
-                                    disabled
-                                    autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-
-                            
-                        </div>
-
-                        <div class="row-span-2 col-span-2 px-8 py-8">
-                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                            {t("profile.Username")}
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    type="text"
-                                    name="username"
-                                    id="username"
-                                    value={username || ''}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                    autoComplete="given-name"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-                        <div class="col-span-2 px-8 py-8">
-                            <label htmlFor="full_name" className="block text-sm font-medium leading-6 text-gray-900">
-                            {t("profile.FullName")}
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    type="text"
-                                    name="full_name"
-                                    id="full_name"
-                                    value={full_name || ''}
-                                    onChange={(e) => setFullname(e.target.value)}
-                                    autoComplete="full_name"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
+        <div className="py-24 bg-white sm:py-32">
+            <div className="px-4 py-4 mx-auto max-w-7xl lg:px-8">
+                <div className="max-w-2xl mx-auto space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
+                    <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
                         
-                        <div class="row-span-2 col-span-2 px-8 py-8">
-                            <label htmlFor="website" className="block text-sm font-medium leading-6 text-gray-900">
-                            {t("profile.Website")}
-                            </label>
-                            <div className="mt-2">
-                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-600 sm:max-w-md">
-                                    <span className="flex items-center pl-3 text-gray-500 select-none sm:text-sm">http://</span>
-                                    <input
-                                    type="url"
-                                    name="website"
-                                    id="website"
-                                    value={website || ''}
-                                    onChange={(e) => setWebsite(e.target.value)}
-                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="www.example.com"
-                                    />
-                                </div>
+                        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+                            <div className="p-10 rounded-2xl bg-gray-50">
+                                <h3 className="text-base font-semibold leading-7 text-gray-900">{t("profile.Email")}</h3>
+                                <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                    <div>
+                                        <dt className="sr-only">Email</dt>
+                                        <dd>
+                                        <input
+                                            id="email" 
+                                            type="text" 
+                                            value={session.user.email} 
+                                            disabled
+                                            autoComplete="email"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                        </dd>
+                                    </div>
+                                </dl>
+                            </div>
+                            <div className="p-10 rounded-2xl bg-gray-50">
+                                <h3 className="text-base font-semibold leading-7 text-gray-900">{t("profile.FullName")}</h3>
+                                <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                    <div>
+                                        <dt className="sr-only">FullName</dt>
+                                        <dd>
+                                        <input
+                                            type="text"
+                                            name="full_name"
+                                            id="full_name"
+                                            value={full_name || ''}
+                                            onChange={(e) => setFullname(e.target.value)}
+                                            autoComplete="full_name"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                        </dd>
+                                    </div>
+                                </dl>
+                            </div>
+                            <div className="p-10 rounded-2xl bg-gray-50">
+                                <h3 className="text-base font-semibold leading-7 text-gray-900">{t("profile.Username")}</h3>
+                                <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                    <div>
+                                        <dt className="sr-only">Username</dt>
+                                        <dd>
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            value={username || ''}
+                                            onChange={(e) => setUsername(e.target.value)}
+                                            autoComplete="given-name"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                        </dd>
+                                    </div>
+                                </dl>
+                            </div>
+                            <div className="p-10 rounded-2xl bg-gray-50">
+                                <h3 className="text-base font-semibold leading-7 text-gray-900">{t("profile.Website")}</h3>
+                                <dl className="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                    <div>
+                                        <dt className="sr-only">Website</dt>
+                                        <dd>
+                                        <input
+                                            type="url"
+                                            name="website"
+                                            id="website"
+                                            value={website || ''}
+                                            onChange={(e) => setWebsite(e.target.value)}
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            placeholder="www.example.com"
+                                            />
+                                        </dd>
+                                    </div>
+                                </dl>
                             </div>
                         </div>
-
-                        <div class="row-span-3 px-8 py-8">
-                           
-                            <Avatar
+                        <div className="text-center">
+                            <h2 className="py-4 text-3xl font-bold tracking-tight text-gray-900">{t("profile.UserImage")}</h2>
+                            <div className="inline-block p-2 border border-gray-300">
+                                <Avatar
                                 uid={user.id}
                                 url={avatar_url}
                                 size={150}
@@ -160,30 +165,31 @@ export default function UserProfile( { session } ) {
                                     setAvatarUrl(url)
                                     updateProfile({ username, website, avatar_url: url })
                                 }}
-                            />
-                           
+                                />
+                            </div>
                         </div>
                     </div>
-              
-             
-                    <div className="flex items-center justify-end px-4 py-4 border-t gap-x-6 border-gray-900/10 sm:px-8">
-                        <button type="button" 
-                            onClick={() => supabase.auth.signOut()}
-                            className="text-sm font-semibold leading-6 text-gray-900">
-                            {t("profile.SignOut")}
-                        </button>
-                        <button
-                            onClick={() => updateProfile({ username,full_name, website, avatar_url })}
-                            disabled={loading}
-                            className="px-3 py-2 text-sm font-semibold text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                        >
-                           {loading ? 'Loading ...' : t('profile.Update')}
-                        
-                        </button>
-                    </div>
-                </form>
+                </div>
+            </div>
+            <div className="flex items-center justify-end px-4 py-4 border-t gap-x-6 border-gray-900/10 sm:px-8">
+                <button type="button" 
+                    onClick={() => supabase.auth.signOut()}
+                    className="text-sm font-semibold leading-6 text-gray-900">
+                    {t("profile.SignOut")}
+                </button>
+                <button
+                    onClick={() => updateProfile({ username,full_name, website, avatar_url })}
+                    disabled={loading}
+                    className="px-3 py-2 text-sm font-semibold text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                >
+                {loading ? 'Loading ...' : t('profile.Update')}
+                
+                </button>
             </div>
         </div>
+        
+
+
     )
 }
 
